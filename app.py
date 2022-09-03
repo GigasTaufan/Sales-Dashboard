82c48c838900a671590d1ee1289a79495129bdc5
gigastaufan <970196@telkom.co.id>
2022-09-03 15:30:48 +0700

    adding sidebar

--- app.py
+++ app.py
@@ -8,5 +8,39 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 
 df = pd.read_excel('supermarkt_sales.xlsx')
 
-st.dataframe(df)
+# st.dataframe(df)
+
+# SIDEBAR
+st.sidebar.header("Filter Here:")
+
+## City filter
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+## Customer type filter
+customer_type = st.sidebar.multiselect(
+    "Select Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique()
+)
+
+## Gender
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+# filtering the selection
+# df_selection = df.query(
+#     "City==@city & Customer_type==@customer_type & Gender==@gender"
+# )
+
+# st.dataframe(df_selection)
+
+
+
 
